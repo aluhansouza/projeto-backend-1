e4ce9454f61fbd2790b17b513341c43444ffcfc2
aluhan.yuri.souza@gmail.com <aluhan.yuri.souza@gmail.com>
2025-07-08 01:15:24 -0400

    Commit 08-07-2025 01:15

--- src/main/resources/db/migration/V18__Inserir_Na_Tabela_Usuario.sql
+++ src/main/resources/db/migration/V18__Inserir_Na_Tabela_Usuario.sql
@@ -2,6 +2,6 @@
 
 -- Copiando dados para a tabela api.tb_usuario: ~2 rows (aproximadamente)
 INSERT INTO `tb_usuario` (`nome`, `user_name`, `password`, `enabled`) VALUES
-  ('Administrador', 'admin@sistema.com', '$2a$10$hashfakeadmin', 1),
-  ('Usuário', 'user@sistema.com', '$2a$10$hashfakeuser', 1),
-  ('Gerente', 'manager@sistema.com', '$2a$10$hashfakemanager', 1);
+  ('Administrador', 'admin@sistema.com', '$2a$12$78J2Gw8VKocuzspxfR3KveTErV8Lcqxcs1jTCrzDY8G3q.jKvdiCm', 1),
+  ('Usuário', 'user@sistema.com', '$2a$12$78J2Gw8VKocuzspxfR3KveTErV8Lcqxcs1jTCrzDY8G3q.jKvdiCm', 1),
+  ('Gerente', 'manager@sistema.com', '$2a$12$78J2Gw8VKocuzspxfR3KveTErV8Lcqxcs1jTCrzDY8G3q.jKvdiCm', 1);
