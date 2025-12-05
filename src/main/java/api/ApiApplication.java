9df21d80596573c413cdab4350d3848980add34d
Aluhan Yuri <aluhan.yuri.souza@gmail.com>
2025-12-05 01:09:10 -0400

    Terceiro Commit

--- src/main/java/api/ApiApplication.java
+++ src/main/java/api/ApiApplication.java
@@ -3,8 +3,19 @@ package api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(
+        /* exclude = {
+                SecurityAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
+        }
+        */
+)
 public class ApiApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
